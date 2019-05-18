d72a3c31051c67bc9bc5f98237efadbb3bf17235
Jonathan Biro <jonnybrx@gmail.com>
2019-05-18 16:59:44 -0400

    updates

--- src/index.js
+++ src/index.js
@@ -15,9 +15,9 @@ class Du4e {
   }
 
   initialize(){
-    const contractAddr = "0x5fc4c5ba2e1d22191fb239a1afe513f7ff892c9c"
+    this.contractAddr = "0x5fc4c5ba2e1d22191fb239a1afe513f7ff892c9c"
     this.web3 = window.web3 || new Web3(Web3.givenProvider)
-    this.contract = new this.web3.eth.contract(this.abi(), contractAddr)
+    this.contract = new this.web3.eth.contract(this.abi())
 
   }
 
@@ -170,7 +170,9 @@ class Du4e {
 
   async listOfUrls(acct){
     let account = acct || web3.eth.accounts[0]
-    const urls = await this.contract.shortenedURLs(account).call()
+    alert(account)
+    let contract = contract.at(this.contractAddr)
+    const urls = contract.shortenedURLs(account).call()
     console.log(urls)
   }
 
