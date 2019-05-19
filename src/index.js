af5758b600f9c3d99ca4d5a35238bdd906cf8bbb
Jonathan Biro <jonnybrx@gmail.com>
2019-05-18 21:25:14 -0400

    updates

--- src/index.js
+++ src/index.js
@@ -1,33 +1,38 @@
 const Web3 = require('web3')
 
 class Du4e {
-  constructor(){
-    this.checkForWeb3 = this.checkForWeb3.bind(this)
-    this.grabShortened = this.grabShortened.bind(this)
-    this.addUrl = this.addUrl.bind(this)
-    this.urlCreated = this.urlCreated.bind(this)
-    this.onTxSend = null
-    this.checkForWeb3()
+  constructor() {
+    this.checkForWeb3 = this.checkForWeb3.bind(this);
+    this.grabShortened = this.grabShortened.bind(this);
+    this.addUrl = this.addUrl.bind(this);
+    this.urlCreated = this.urlCreated.bind(this);
+    this.onTxSend = null;
+    this.checkForWeb3();
   }
 
-  checkForWeb3(){
+  checkForWeb3() {
     if (window.web3 && web3.currentProvider && window.ethereum) {
-      this.initialize()
-      return
+      this.initialize();
+      return;
     }
-    window.setTimeout(this.checkForWeb3, 100)
+    window.setTimeout(this.checkForWeb3, 100);
   }
 
-  initialize(){
+  initialize() {
     ethereum.enable().then(() => {
       this.contractAddr = "0x19624ffa41fe26744e74fdbba77bef967a222d4c";
-      this.web3 = window.web3 || new Web3(Web3.givenProvider)
-      if (this.web3.version.api.startsWith("0")){
-        this.contract = this.web3.eth.contract(this.abi()).at(this.contractAddr)
+      this.web3 = window.web3 || new Web3(Web3.givenProvider);
+      if (this.web3.version.api.startsWith("0")) {
+        this.contract = this.web3.eth
+          .contract(this.abi())
+          .at(this.contractAddr);
       } else {
-        this.contract = new this.web3.eth.Contract(this.abi(), this.contractAddr)
+        this.contract = new this.web3.eth.Contract(
+          this.abi(),
+          this.contractAddr
+        );
       }
-    })
+    });
   }
 
   abi() {
@@ -166,75 +171,91 @@ class Du4e {
         stateMutability: "view",
         type: "function"
       }
-    ]
+    ];
   }
 
-  grabShortened(acct, index = 0, cb, urls) {
+  grabShortened(acct, index = 0, cb, urls, devCb) {
     this.contract.shortenedURLs(acct, index, (x, short) => {
-      this.addUrl(short, urls, acct, index + 1)
-    })
+      this.addUrl(short, urls, acct, index + 1, devCb);
+    });
   }
 
-  addUrl(short, urls, acct, index) {
+  addUrl(short, urls, acct, index, devCb) {
     if (short) {
-      urls.push(short)
-      this.grabShortened(acct, index, this.addUrl, urls)
+      urls.push(short);
+      this.grabShortened(acct, index, this.addUrl, urls, devCb);
+    } else {
+      devCb(urls)
     }
   }
 
-  urlCreated(x, txId){
-    let event = new Event("urlCreated")
-    if(this.onTxSend){
-      this.onTxSend(txId)
+  urlCreated(x, txId) {
+    let event = new Event("urlCreated");
+    if (this.onTxSend) {
+      this.onTxSend(txId);
     }
   }
 
-  async shortenUrl(url, opts={}) {
-    let { slug, acct, cb } = opts
-    let account = acct || web3.eth.accounts[0]
-    let tx = { from: account }
+  async shortenUrl(url, opts = {}) {
+    let { slug, acct, cb } = opts;
+    let account = acct || web3.eth.accounts[0];
+    let tx = { from: account };
 
     if (this.web3.version.api.startsWith("0")) {
-      slug ?
-        this.contract.shortenURLWithSlug.sendTransaction(url, slug, true, tx, this.urlCreated) :
-        this.contract.shortenURL.sendTransaction(url, true, tx, this.urlCreated)
+      slug
+        ? this.contract.shortenURLWithSlug.sendTransaction(
+            url,
+            slug,
+            true,
+            tx,
+            this.urlCreated
+          )
+        : this.contract.shortenURL.sendTransaction(
+            url,
+            true,
+            tx,
+            this.urlCreated
+          );
     } else {
-      slug ?
-        this.contract.methods.shortenURLWithSlug(url, slug, true).send(tx, this.urlCreated) :
-        this.contract.shortenURL(url, true).send(tx, this.urlCreated)
+      slug
+        ? this.contract.methods
+            .shortenURLWithSlug(url, slug, true)
+            .send(tx, this.urlCreated)
+        : this.contract.shortenURL(url, true).send(tx, this.urlCreated);
     }
   }
 
-  async getUrl(slug, cb){
+  async getUrl(slug, cb) {
     if (this.web3.version.api.startsWith("0")) {
-      this.contract.getURL.call(slug, cb)
-      return true
+      this.contract.getURL.call(slug, cb);
+      return true;
     } else {
-      const destination = await this.contract.methods.getURL(slug).call()
+      const destination = await this.contract.methods.getURL(slug).call();
     }
-    return destination
+    return destination;
   }
 
-  async listOfUrls(acct){
-    let account = acct || web3.eth.accounts[0]
-    const urls = []
+  async listOfUrls(acct, cb) {
+    let account = acct && typeof(account) == 'string' || web3.eth.accounts[0];
+    const urls = [];
     if (this.web3.version.api.startsWith("0")) {
-      this.grabShortened(account, 0, this.addUrl, urls)
+      let callback = typeof(acct) == 'function' ? acct : cb
+      this.grabShortened(account, 0, this.addUrl, urls, callback);
     } else {
-      let url = await this.contract.shortenedURLs(account, 0).call()
-      let i  = 1
-      while (url){
-        url = await this.contract.shortenedURLs(account, i).call()
-        urls.push(url)
-        i += 1
+      let url = await this.contract.shortenedURLs(account, 0).call();
+      let i = 1;
+      while (url) {
+        url = await this.contract.shortenedURLs(account, i).call();
+        urls.push(url);
+        i += 1;
       }
     }
 
-    return urls
+    return urls;
   }
 
-  async forward(slug){
-    window.location.href = await this.getUrl(slug)
+  async forward(slug) {
+    window.location.href = await this.getUrl(slug);
   }
 }
 
